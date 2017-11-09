406b9973928fd86650e532d784b49bf99be81fd8
jakedub <jacob.w.moore@gmail.com>
2017-11-08 19:20:25 -0500

    updating front end

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171107125119) do
+ActiveRecord::Schema.define(version: 20171108184346) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "caption"
@@ -20,6 +30,8 @@ ActiveRecord::Schema.define(version: 20171107125119) do
     t.string "image_content_type"
     t.integer "image_file_size"
     t.datetime "image_updated_at"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,9 +47,10 @@ ActiveRecord::Schema.define(version: 20171107125119) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index [nil], name: "index_users_on_user_name", unique: true
+    t.index ["user_name"], name: "index_users_on_user_name", unique: true
   end
 
 end
